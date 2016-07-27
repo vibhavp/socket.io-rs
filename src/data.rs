130198b47787556341fd675212f51e82c8a6bea0
Vibhav Pant <vibhavp@gmail.com>
2016-07-27 19:15:20 +0530

    Replace decoder with data

---
+++ src/data.rs
@@ -0,0 +1,31 @@
+use serde_json::Value;
+use serde_json::de::from_str;
+
+pub enum Data {
+    JSON(Value),
+    Binary(Vec<u8>)
+}
+
+#[doc(hidden)]
+pub fn encode_data(data: Vec<Data>) -> (Value, Vec<Vec<u8>>) {
+    let mut json = vec![];
+    let mut binary = vec![];
+    let mut placeholder_num = 0;
+
+    for value in data {
+        json.push(match value {
+            Data::JSON(v) => v,
+            Data::Binary(b) => {
+                binary.push(b);
+                placeholder_num = placeholder_num+1;
+                placeholder(placeholder_num)
+            }
+        })
+    }
+
+    (Value::Array(json), binary)
+}
+
+fn placeholder(num: usize) -> Value {
+    from_str(&format!("{{\"_placeholder\":true,\"num\": {}}}", num)).unwrap()
+}
